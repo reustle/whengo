46913acee4f3c64ae4f930d8a44461edb3bda986
Shane Reustle <me@shanereustle.com>
2015-03-13 10:51:37 +0700

    sudo make me a sandwich

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" == 'build' ]; then
-	docker build -t whengoio .
+	sudo docker build -t whengoio .
 elif [ "$1" == 'run' ]; then
-	docker run -d --name="whengoio" `cat settings.txt` whengoio
+	sudo docker run -d --name="whengoio" `cat settings.txt` whengoio
 else
 	echo 'Usage: ./docker.sh [build|run]'
 fi
