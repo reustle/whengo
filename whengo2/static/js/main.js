982d6ca209d26adab3fbfc6f1177dcd7a12eb520
Shane Reustle <me@shanereustle.com>
2015-10-02 15:01:26 +0900

    always redraw instead of filter

--- whengo2/static/js/main.js
+++ whengo2/static/js/main.js
@@ -1,6 +1,8 @@
 // Map
 var drawMarkers = function(){
 	
+	console.time('drawMarkers');
+	
 	// Create a new mapbox FeatureCollection
 	var markers = {
 		type: 'FeatureCollection',
@@ -8,7 +10,8 @@ var drawMarkers = function(){
 	};
 	
 	// Create and insert a marker for each result airport into the FeatureCollection
-	_.each(airportData, function(airport){
+	var airports = queryAirports();
+	_.each(airports, function(airport){
 		
 		markers.features.push({
 			type : 'Feature',
@@ -29,8 +32,21 @@ var drawMarkers = function(){
 	// Update map with the new data
 	mapMarkers.setGeoJSON(markers);
 	
-	// Apply the filters
-	filterMarkers();
+	console.timeEnd('drawMarkers');
+	
+	// Create a custom marker icon (circle icon)
+	var markerIcon = L.icon({
+		iconUrl : 'http://whengo.io/dot.png',
+		iconSize : [9,9],
+		iconAnchor : [4,4]
+	});
+	
+	// Go through and update each icon with the new icon (lame)
+	map.eachLayer(function(marker){
+		if(marker.setIcon){
+			marker.setIcon(markerIcon);
+		}
+	});
 	
 };
 
@@ -64,10 +80,10 @@ var queryAirports = function(){
 			return false;
 		}
 		
-		if(airport.lon < mapNW.lng && airport.lon > mapSE.lng){
+		if(airport.lon < mapNW.lng || airport.lon > mapSE.lng){
 			return false;
 		}
-		if(airport.lat < mapSE.lat && airport.lat > mapNW.lat){
+		if(airport.lat < mapSE.lat || airport.lat > mapNW.lat){
 			return false;
 		}
 		
@@ -77,46 +93,43 @@ var queryAirports = function(){
 	
 	console.log('Results:', results.length);
 	
-	return _.pluck(results, 'ia');
+	return results;
 	
 }
 
-var filterMarkers = function(){
-	// Filter the mapbox marker instances
+var initMap = function(){
 	
+	// Initialize MapBox
+	L.mapbox.accessToken = 'pk.eyJ1IjoicmV1c3RsZSIsImEiOiJESzd6YVRnIn0.Hh9AwQw1X0PR_TOewZMMzA';
+	map = L.mapbox.map('mapContainer', 'reustle.l8pgo1n1').setView([11.86, 0], 2);
+	mapMarkers = L.mapbox.featureLayer().addTo(map);
 	
-	// Get the list of airports we should show
-	var visibleAirports = queryAirports();
+	// Add mapbox events
+	map.on('dragend', function(){
+		drawMarkers();
+	});
+	map.on('zoomend', function(){
+		drawMarkers();
+	});
+	map.on('resize', function(){
+		drawMarkers();
+	});
 	
-	console.time('filterMarkers');
+	mapMarkers.on('click', function(e){
+		// TODO
+		//Session.set('selectedAirport', e.layer.feature.properties.airportId);
+		//Modal.show('detailsModal');
+	});
 	
-	// Filter the markers
-	mapMarkers.setFilter(function(marker){
-		if(visibleAirports.indexOf(marker.properties.airportId) !== -1){
-			return true;
+	var drawOnceMapboxExists = function(){
+		if(typeof(window.mapMarkers) != 'undefined'){
+			drawMarkers();
+		}else{
+			setTimeout(drawOnceMapboxExists, 250);
 		}
-	});
+	}
 	
-	console.timeEnd('filterMarkers');
-	
-	
-	// Create a custom marker icon (circle icon)
-	var markerIcon = L.icon({
-		iconUrl : 'http://whengo.io/dot.png',
-		iconSize : [9,9],
-		iconAnchor : [4,4]
-	});
-	
-	console.time('setIcons');
-	
-	// Go through and update each icon with the new icon (lame)
-	map.eachLayer(function(marker){
-		if(marker.setIcon){
-			marker.setIcon(markerIcon);
-		}
-	});
-	
-	console.timeEnd('setIcons');
+	drawOnceMapboxExists();
 	
 }
 
@@ -135,37 +148,8 @@ var setDefaults = function(){
 
 // Init
 
+var map, mapMarkers;
+
 setDefaults();
-
-// Initialize MapBox
-L.mapbox.accessToken = 'pk.eyJ1IjoicmV1c3RsZSIsImEiOiJESzd6YVRnIn0.Hh9AwQw1X0PR_TOewZMMzA';
-var map = L.mapbox.map('mapContainer', 'reustle.l8pgo1n1').setView([11.86, 0], 2);
-var mapMarkers = L.mapbox.featureLayer().addTo(map);
-
-// Add mapbox events
-map.on('dragend', function(){
-	filterMarkers();
-});
-map.on('zoomend', function(){
-	filterMarkers();
-});
-map.on('resize', function(){
-	filterMarkers();
-});
-
-mapMarkers.on('click', function(e){
-	// TODO
-	//Session.set('selectedAirport', e.layer.feature.properties.airportId);
-	//Modal.show('detailsModal');
-});
-
-var drawOnceMapboxExists = function(){
-	if(typeof(window.mapMarkers) != 'undefined'){
-		drawMarkers();
-	}else{
-		setTimeout(drawOnceMapboxExists, 250);
-	}
-}
-
-drawOnceMapboxExists();
+initMap();
 
